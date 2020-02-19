c6639bc0fb8a5eea43e0ea17ec979fa072ee4d89
Nevoraz <53296375+Nevoraz@users.noreply.github.com>
2020-02-19 21:16:04 +0200

    Add files via upload

---
+++ pcc_client.c
@@ -0,0 +1,85 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <sys/stat.h>
+void send_data(int sockfd, char * filePath);
+
+int main(int argc, char *argv[])
+{
+    int  sockfd     = -1;
+    char recv_buff[1024];
+
+    struct sockaddr_in serv_addr; // where we Want to get to
+    struct sockaddr_in my_addr;   // where we actually connected through
+    struct sockaddr_in peer_addr; // where we actually connected to
+    socklen_t addrsize = sizeof(struct sockaddr_in );
+
+    memset(recv_buff, 0,sizeof(recv_buff));
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        strerror(errno);
+        exit(1);
+    }
+
+    // print socket details
+    getsockname(sockfd,
+                (struct sockaddr*) &my_addr,
+                &addrsize);
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(atoi(argv[2])); // Note: htons for endiannes
+//    inet_aton(argv[1], &serv_addr.sin_addr);
+    inet_pton(AF_INET, argv[1], &serv_addr.sin_addr);
+
+    // Note: what about the client port number?
+    // connect socket to the target address
+    if( connect(sockfd,
+                (struct sockaddr*) &serv_addr,
+                sizeof(serv_addr)) < 0)
+    {
+        strerror(errno);
+        exit(1);
+    }
+    // print socket details again
+    getsockname(sockfd, (struct sockaddr*) &my_addr,   &addrsize);
+    getpeername(sockfd, (struct sockaddr*) &peer_addr, &addrsize);
+    send_data(sockfd, argv[3]);
+    close(sockfd); // is socket really done here?
+    return 0;
+}
+
+
+void send_data(int sockfd, char * filePath){
+    int N = 0;
+    char C[sizeof(int)];
+    char *buffer;
+    FILE *fp;
+    struct stat st;
+    // opening the file
+    if ((fp = fopen(filePath,"r")) == NULL){
+        strerror(errno);
+        exit(1);
+    }
+    stat(filePath, &st);
+    N = (int)st.st_size;
+    buffer = malloc(N * sizeof(char));
+    sprintf(buffer, "%d", N);
+    // sending N, the size of the file
+    write(sockfd, buffer, sizeof(int));
+    fread(buffer, N, 1, fp);
+    // sending the data
+    write(sockfd, buffer, N);
+    free(buffer);
+    fclose(fp);
+    //    reading input from server
+    read(sockfd, C, sizeof(int));
+    printf("# of printable characters: %u\n", atoi(C));
+}
---
+++ pcc_server.c
@@ -0,0 +1,137 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <assert.h>
+
+int counts[127];
+int counts_tmp[127];
+int sigintFlag = 0; // if SIGINT was recived so it equals 1
+
+void read_data(int *counts, int connfd);
+int count_data(int *counts, char *buffer, int N);
+void print_counts(int* counts);
+void combine_counts(int *counts, int *counts_tmp);
+
+void sigint_handler_flag(int sig){
+    sigintFlag = 1;
+}
+
+void sigint_handler_exit(int sig){
+    print_counts(counts);
+    exit(0);
+}
+
+void general_handler(int sig){
+    strerror(errno);
+}
+
+int main(int argc, char *argv[])
+{
+    int listenfd  = -1;
+    int connfd    = -1;
+    int i;
+    int enable = 1;
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in my_addr;
+    struct sockaddr_in peer_addr;
+    socklen_t addrsize = sizeof(struct sockaddr_in );
+    signal(SIGINT, sigint_handler_exit);
+    signal(ETIMEDOUT, general_handler);
+    signal(ECONNRESET, general_handler);
+    signal(EPIPE, general_handler);
+    for (i = 0 ; i < 127 ; i++ ){
+        counts[i] = 0;
+        counts_tmp[i] = 0;
+    }
+    listenfd = socket( AF_INET, SOCK_STREAM, 0 );
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+    memset( &serv_addr, 0, addrsize );
+    serv_addr.sin_family = AF_INET;
+    // INADDR_ANY = any local machine address
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+    
+    if( 0 != bind( listenfd,
+                  (struct sockaddr*) &serv_addr,
+                  addrsize ) ){
+        strerror(errno);
+        exit(1);
+    }
+    if( 0 != listen( listenfd, 10 ) ){
+        strerror(errno);
+        exit(1);
+    }
+    while( 1 ){
+        // Accept a connection.
+        // Can use NULL in 2nd and 3rd arguments
+        // but we want to print the client socket details
+        connfd = accept( listenfd,
+                        (struct sockaddr*) &peer_addr,
+                        &addrsize);
+        signal(SIGINT, sigint_handler_flag);
+        if( connfd < 0 ){
+            strerror(errno);
+            exit(1);
+        }
+        getsockname(connfd, (struct sockaddr*) &my_addr,   &addrsize);
+        getpeername(connfd, (struct sockaddr*) &peer_addr, &addrsize);
+        read_data(counts_tmp, connfd);
+        close(connfd);
+        combine_counts(counts, counts_tmp);
+        if (sigintFlag == 1){
+            print_counts(counts);
+            exit(0);
+        }
+        signal(SIGINT, sigint_handler_exit);
+    }
+}
+
+void read_data(int *counts_tmp, int connfd){
+    int C;
+    char N[sizeof(int)];
+    char *buffer;
+    // read the message from client and copy it in buffer
+    read(connfd, N, sizeof(int));
+    buffer = malloc(atoi(N) * sizeof(char));
+    read(connfd, buffer, atoi(N));
+    C = count_data(counts, buffer, atoi(N));
+    // and send that buffer to client
+    sprintf(buffer, "%d", C);
+    write(connfd, buffer, sizeof(int));
+    free(buffer);
+}
+
+int count_data(int *counts, char *buffer, int N){
+    int i;
+    int result = 0;
+    for (i = 0 ; i < N ; i++ ){
+        if ((int)buffer[i] >= 32 && (int)buffer[i] <= 126){
+            result++;
+            counts[(int)buffer[i]]++;
+        }
+    }
+    return result;
+}
+
+void print_counts(int* counts){
+    int i;
+    char c;
+    for (i = 32 ; i <= 126 ; i++ ){
+        c = i;
+        printf("char ’%c’ : %u times\n", c, counts[i]);
+    }
+}
+
+
+void combine_counts(int *counts, int *counts_tmp){
+    int i;
+    for (i = 0 ; i < 126 ; i++ )
+        counts[i] = counts[i] + counts_tmp[i];
+}
